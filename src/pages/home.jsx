4436a0bee2b5e66669d87afaee7c7e9b5ae9c412
MikaelGeraldius <ops.gege.lie@gmail.com>
2025-05-09 14:13:07 +0700

    second changes

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,10 +1,82 @@
 import React from 'react'
 import { useState } from 'react'
+import { useReducer } from 'react';
+
+
+function reducer(state, action){
+  let newCount;
+  switch(action.type){
+    case "update-input":
+      return {...state, input: action.payload.value};
+    case "increment":
+      newCount = state.count + 1;
+      return{
+        ...state,
+        count: newCount,
+        blueText: newCount >= 10
+      };
+    case "decrement":
+      newCount = state.count - 1;
+      return{
+        ...state,
+        count: newCount,
+        blueText: newCount >= 10
+      }
+    case "toggle-blue":
+      return{...state, blueText: !state.blueText};
+    default:
+      return state;
+  }
+}
 
 function Home({user}){
+    const [textAreaValue, setTextAreaValue] = useState('');
+    const [selectValue, setSelectValue] = useState('');
+    const [state, dispatch] = useReducer(reducer, {
+      input: '',
+      count: 0,
+      blueText: false
+    });
+
     return (
         <div className="text-center">
-          <h1 className="text-3xl font-bold mb-4">Welcome, {user.username}!</h1>
+          <h1 className="text-3xl font-bold mb-4">Hello I'm {user.username}, I'm {user.age} and my hobby is {user.hobby}! {selectValue && "I like " + selectValue + "!"} {textAreaValue && "I Typed : " + textAreaValue}</h1>
+
+          <select
+            onChange={(e) => setSelectValue(e.target.value)}
+            value={selectValue} className='text-3xl border-1 rounded-2xl my-5 bg-[#242424]'
+          >
+            <option value='Tung Tung Tung Sahur'>Tung Tung Tung Sahur</option>
+            <option value='Bombardino Coccodrillo'>Bombardino Coccodrillo</option>
+            <option value='Tralalero Tralala'>Tralalero Tralala</option>
+          </select>
+
+          <textarea
+            cols='50' rows='10' value={textAreaValue} className='border-1 rounded-2xl p-1 mb-10'
+            onChange={(e) => setTextAreaValue(e.target.value)}
+          />
+
+          <hr/>
+
+          <h1 className='m-5'>Use Reducer</h1>
+          <div>
+            <input
+              className='border-1 text-2xl p-1 m-5'
+              type="text" value={state.input}
+              onChange={(e) => dispatch({
+                type: "update-input",
+                payload: {value: e.target.value}
+              })}
+            />
+            <br/>
+            <button className='mx-3' onClick={() => dispatch({type: "increment"})}>+</button>
+            <button className='mx-3' onClick={() => dispatch({type: "decrement"})}>-</button>
+            <button className='mx-3' onClick={() => dispatch({type: "toggle-blue"})}>Toggle Blue Text</button>
+            <div style={{color: state.blueText && "blue"}}>
+              <h2 className='text-3xl'>Count: {state.count}</h2>
+              <h2 className='text-3xl'>Input: {state.input}</h2>
+            </div>
+          </div>
         </div>
     );
 }
--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { useState } from 'react';
 
 export default function SignInForm({ onSignIn }) {
-  const [formData, setFormData] = useState({ username: '', password: '' });
+  const [formData, setFormData] = useState({ username: '', password: '' , hobby: '', age: ''});
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -20,8 +20,7 @@ export default function SignInForm({ onSignIn }) {
       className="text-black bg-white p-8 rounded-xl shadow-md w-80 space-y-6"
     >
       <h1 className="text-2xl font-semibold text-center">Sign In</h1>
-
-      {/* Username Field */}
+      
       <div className="relative">
         <input
           type="text"
@@ -40,7 +39,42 @@ export default function SignInForm({ onSignIn }) {
         </label>
       </div>
 
-      {/* Password Field */}
+      <div className="relative">
+        <input
+          type="text"
+          name="age"
+          value={formData.age}
+          onChange={handleChange}
+          required
+          placeholder="Age"
+          className="peer w-full border-b-2 border-gray-300 focus:border-blue-500 focus:outline-none bg-transparent pt-6 pb-1 text-gray-900 placeholder-transparent"
+        />
+        <label
+          htmlFor="age"
+          className="absolute left-0 top-1 text-gray-500 text-sm transition-all peer-placeholder-shown:top-6 peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-400 peer-focus:top-1 peer-focus:text-sm peer-focus:text-blue-500"
+        >
+          Age
+        </label>
+      </div>
+
+      <div className="relative">
+        <input
+          type="text"
+          name="hobby"
+          value={formData.hobby}
+          onChange={handleChange}
+          required
+          placeholder="Hobby"
+          className="peer w-full border-b-2 border-gray-300 focus:border-blue-500 focus:outline-none bg-transparent pt-6 pb-1 text-gray-900 placeholder-transparent"
+        />
+        <label
+          htmlFor="hobby"
+          className="absolute left-0 top-1 text-gray-500 text-sm transition-all peer-placeholder-shown:top-6 peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-400 peer-focus:top-1 peer-focus:text-sm peer-focus:text-blue-500"
+        >
+          Hobby
+        </label>
+      </div>
+
       <div className="relative">
         <input
           type="password"
